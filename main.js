7f06caac4657751397148a7fe01adcfefe705eaf
pavelcdn <pavelcdn@gmail.com>
2021-09-20 19:39:03 +0300

    test 1 done

--- main.js
+++ main.js
@@ -5,3 +5,18 @@ const pandemicStartMap = "XX0X10010X000X010X0";
 
 infectedArr = pandemicStartMap.split('');
 const start = document.querySelector('.pandemicStart');
+
+function infection(people) {
+    for (let i = 0; i <= people.length; i++) {
+        let cube = document.createElement('div');
+        if (people[i] == "X") {
+            start.appendChild(cube).classList.add("ocean");
+        } else if (people[i] == 1) {
+            start.appendChild(cube).classList.add("infected");
+        } else if (people[i] == 0) {
+            start.appendChild(cube).classList.add("uninfected");
+        }
+    }
+}
+
+infection(infectedArr);
