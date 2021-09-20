416977be7d99c1f3a14e3aa515d1631b6957a3c7
pavelcdn <pavelcdn@gmail.com>
2021-09-20 08:05:50 +0300

    save

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 const pandemicStartMap = "XX0X10010X000X010X0";
+// 0 - uninfected
+// 1 - infected
+// Х - ocean
+
+infectedArr = pandemicStartMap.split('');
+const start = document.querySelector('.pandemicStart');
