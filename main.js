ee473b846c03b1f7dd184666e8e2adab04719937
pavelcdn <pavelcdn@gmail.com>
2021-09-21 15:46:33 +0300

    test 2 done

--- main.js
+++ main.js
@@ -3,20 +3,47 @@ const pandemicStartMap = "XX0X10010X000X010X0";
 // 1 - infected
 // Х - ocean
 
-infectedArr = pandemicStartMap.split('');
-const start = document.querySelector('.pandemicStart');
+let infectedArr = pandemicStartMap.split('');
+const pandemicStart = document.querySelector('.pandemicStart');
+const pandemicEnd = document.querySelector('.pandemicEnd');
 
-function infection(people) {
+function infection(people, div) {
     for (let i = 0; i <= people.length; i++) {
         let cube = document.createElement('div');
         if (people[i] == "X") {
-            start.appendChild(cube).classList.add("ocean");
+            div.appendChild(cube).classList.add("ocean");
         } else if (people[i] == 1) {
-            start.appendChild(cube).classList.add("infected");
+            div.appendChild(cube).classList.add("infected");
         } else if (people[i] == 0) {
-            start.appendChild(cube).classList.add("uninfected");
+            div.appendChild(cube).classList.add("uninfected");
         }
     }
 }
 
-infection(infectedArr);
+infection(infectedArr, pandemicStart);
+
+
+for (let i = 0; i < infectedArr.length; i++) {
+    if (infectedArr[i] == 0 || infectedArr[i] == 1) {
+        if (infectedArr[i] == 1) {
+            for (let j = i; j >= 0; j--) {
+                if (infectedArr[j] == "X") {
+                    break;
+                } else {
+                    for (let k = j; k < infectedArr.length; k++) {
+                        if (infectedArr[k] == 0 || infectedArr[k] == 1) {
+                            infectedArr.fill("1", start = k, end = k + 1);
+                        } else {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+infection(infectedArr, pandemicEnd);
+
+
